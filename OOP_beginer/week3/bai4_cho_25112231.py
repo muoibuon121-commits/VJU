b060f68cf79f8e4fc553d2ad81db76f567f7654d
Muối <muoibuon121@gmail.com>
2026-04-08 11:07:13 +0700

    Update bai4_cho_25112231.py

--- OOP_beginer/week3/bai4_cho_25112231.py
+++ OOP_beginer/week3/bai4_cho_25112231.py
@@ -9,13 +9,13 @@ class Cho:
         cmd = input(f"Làm gì với {self.ten}? (chọc/ăn/vỗ): ").lower()
         if cmd == "chọc":
             self.cam_xuc = "tức giận"
-            print(f"💢 {self.ten} đang {self.cam_xuc}, CHẠY MAU!")
+            print(f" {self.ten} đang {self.cam_xuc}, CHẠY MAU!")
         elif cmd == "ăn":
             self.cam_xuc = "hạnh phúc"
-            print(f"🦴 {self.ten} đang {self.cam_xuc}, vẫy đuôi tít mù!")
+            print(f" {self.ten} đang {self.cam_xuc}, vẫy đuôi tít mù!")
         elif cmd == "vỗ":
             self.cam_xuc = "vui vẻ"
-            print(f"🐶 {self.ten} đang {self.cam_xuc}, nằm ngửa chờ xoa bụng.")
+            print(f" {self.ten} đang {self.cam_xuc}, nằm ngửa chờ xoa bụng.")
 
 if __name__ == '__main__':
     my_dog = Cho("Lu", "Vàng", "Cỏ")
