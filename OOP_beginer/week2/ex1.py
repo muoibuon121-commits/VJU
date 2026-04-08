e1fabbcdd93abe55f6560f70b1e793ff90ce6399
Muối <muoibuon121@gmail.com>
2026-04-08 10:55:59 +0700

    Delete OOP_beginer/week2/ex1.py

--- OOP_beginer/week2/ex1.py
+++
@@ -1,8 +0,0 @@
-def tinh_giay():
-    print("--- 1.1: Đổi phút giây sang giây ---")
-    m = float(input("Nhập số phút: "))
-    n = int(input("Nhập số giây: "))
-    tong_giay = m * 60 + n
-    print(f"Tổng cộng: {tong_giay} giây")
-
-tinh_giay()
