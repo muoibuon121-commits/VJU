9973e7d191f5db487d6f1910cdba4741ded8f1e3
Muối <muoibuon121@gmail.com>
2026-04-08 11:07:28 +0700

    Update bai4_xe_25112231.py

--- OOP_beginer/week3/bai4_xe_25112231.py
+++ OOP_beginer/week3/bai4_xe_25112231.py
@@ -4,12 +4,12 @@ class Car:
         self.gia = gia
 
     def lai_xe(self):
-        print(f"\n🚗 Đang lái chiếc {self.hang}...")
+        print(f"\n Đang lái chiếc {self.hang}...")
         lua_chon = input("Gặp xe đi láo, đâm không? (có/không): ").lower()
         if lua_chon == "có":
-            print("💥 RẦM! Xe có bảo hiểm, lo gì!")
+            print(" RẦM! Xe có bảo hiểm, lo gì!")
         else:
-            print("😇 Nhẫn nhịn là quốc sách, a di đà phật.")
+            print(" Nhẫn nhịn là quốc sách, a di đà phật.")
 
 if __name__ == '__main__':
     my_car = Car("VinFast", "1.2 Tỷ")
